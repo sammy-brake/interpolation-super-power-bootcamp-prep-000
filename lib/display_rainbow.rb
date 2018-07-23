342c29d0a113d05c15cb5855f4e6545ea8bcffd5
Samantha brake <sammy.brake@gmail.com>
2018-07-23 17:56:47 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   puts "R: #{colors[0]}"
+  puts "R: #{colors[0]}"
+  puts "R: #{colors[0]}"
+  puts "R: #{colors[0]}"
+  uts "R: #{colors[0]}"
+  uts "R: #{colors[0]}"
+  uts "R: #{colors[0]}"
 end 
